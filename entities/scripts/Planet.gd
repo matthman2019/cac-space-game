1bf65bd877baa23a66c78d4bcd27a694dd94501e
bassyreflex <nathan@hornskeith.com>
2026-03-25 17:56:23 -0400

    added base constants for planet

--- entities/scripts/Planet.gd
+++ entities/scripts/Planet.gd
@@ -1,6 +1,13 @@
 class_name Planet 
 extends Sprite2D
 
+var sunDistance: float
+var mass: float
+var orbitVel: float
+
+var firstColonizable: bool
+
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
