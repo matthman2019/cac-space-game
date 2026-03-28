e82a3ca0b891ab0c9e9cf3a89f35257fcb5eb3b7
matthman2019 <matthman2019@gmail.com>
2026-03-28 15:20:54 -0400

    Randomly Generated Planets are now shaded
    Replaced regular planet sprites with randomly shaded sprites. Also the planet script now auto-loads sprites (in the assets/grayscalePlanets folder)

--- entities/scripts/Planet.gd
+++ entities/scripts/Planet.gd
@@ -4,6 +4,7 @@ extends Node2D
 var MIN_SPIN_SPEED: float = 0.3
 var MAX_SPIN_SPEED: float = 1.0
 
+'''
 var text1 = load("res://assets/planets/Large red Gas + rings.png")
 var text2 = load("res://assets/planets/Large red Gas.png")
 var text3 = load("res://assets/planets/Pink gass 25 by 25.png")
@@ -11,9 +12,10 @@ var text4 = load("res://assets/planets/planet1.png")
 var text5 = load("res://assets/planets/Rocky 15 pix.png")
 var text6 = load("res://assets/planets/Tidaly Locked small.png")
 var text7 = load("res://assets/planets/Yello + blue gass.png")
+'''
 
-var TEXTURE_LIST = [text1,text2,text3,text4,text5,text6,text7]
-@onready var Sprite: Sprite2D = $Sprite
+var TEXTURE_LIST = []
+@onready var Sprite: Sprite2D = $ShadedPlanet
 
 var planet_size: int = 0
 var planet_temperature: int = 0
@@ -50,7 +52,7 @@ func _process(delta: float) -> void:
 func _ready():
 	$Area2D.connect("input_event", _on_static_body_2d_input_event)
 	$Area2D.connect("mouse_exited", _on_static_body_2d_mouse_exited)
-	
+	load_planet_textures()
 	Sprite.texture = TEXTURE_LIST.pick_random()
 	
 func _on_static_body_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
@@ -62,3 +64,11 @@ func _on_static_body_2d_input_event(viewport: Node, event: InputEvent, shape_idx
 
 func _on_static_body_2d_mouse_exited() -> void:
 	click = false
+
+func load_planet_textures():
+	var textureLocation = "res://assets/grayscalePlanets/"
+	var fileAccess = DirAccess.open(textureLocation)
+	for file in fileAccess.get_files():
+		if file.ends_with(".import"):
+			continue
+		TEXTURE_LIST.append(load(textureLocation.path_join(file)))
--- entities/scripts/shaded_planet_sprite_test.gd
+++ entities/scripts/shaded_planet_sprite_test.gd
@@ -1,14 +1,16 @@
 extends Sprite2D
 
-
+func shuffleColor():
+	var shaderMaterial : ShaderMaterial = material
+	var darkColor : Vector3 = Vector3(GlobalRNG.rng.randf_range(0, 0.5), GlobalRNG.rng.randf_range(0, 0.5), GlobalRNG.rng.randf_range(0, 0.5))
+	var lightColor : Vector3 = Vector3(1, 1, 1) - darkColor #  Vector3(GlobalRNG.rng.randf_range(.5, 1), GlobalRNG.rng.randf_range(.5, 1), GlobalRNG.rng.randf_range(.5, 1))
+	if shaderMaterial:
+		self.set_instance_shader_parameter("darkColor", darkColor)
+		self.set_instance_shader_parameter("lightColor", lightColor)
+	
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var shaderMaterial : ShaderMaterial = material
-	var darkColor : Vector3 = Vector3(randf_range(0, 0.5), randf_range(0, 0.5), randf_range(0, 0.5))
-	var lightColor : Vector3 = Vector3(randf_range(.5, 1), randf_range(.5, 1), randf_range(.5, 1))
-	if shaderMaterial:
-		shaderMaterial.set_shader_parameter("darkColor", darkColor)
-		shaderMaterial.set_shader_parameter("lightColor", lightColor)
+	shuffleColor()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
