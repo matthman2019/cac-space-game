7f650391ae4b3134769680da9f9d42c72bbd5ca3
matthman2019 <matthman2019@gmail.com>
2026-04-01 20:04:20 -0400

    Music
    Added music!

---
+++ entities/scripts/musicPlayer.gd
@@ -0,0 +1,86 @@
+extends AudioStreamPlayer
+# I took this script from an old project
+
+var audio_dict = {}
+var audio_folder_string = "res://assets/audio"
+@onready var audio_folder = DirAccess.open(audio_folder_string)
+
+@onready var fade_duration:float = 2.0
+
+var current_song_type_playing = null
+
+func _ready() -> void:
+	var music_categories = audio_folder.get_directories()
+	
+	for category in music_categories:
+		audio_dict[category] = []
+		var category_dir_string = audio_folder_string.path_join(category)
+		var category_dir = DirAccess.open(category_dir_string)
+		for file in category_dir.get_files():
+			if file.ends_with(".import"):
+				continue
+			audio_dict[category].append(category_dir_string.path_join(file))
+	
+	change_song_instant()
+	
+	finished.connect(change_song_instant)
+	# Globals.changeSong.connect(change_song_to_with_fade)
+	
+func change_song_with_fade(song_type : String = "Background"):
+	if song_type == current_song_type_playing:
+		return
+	
+	# fadeout
+	var fade_out_tween = create_tween()
+	fade_out_tween.tween_property(self, "volume_db", -80, fade_duration)
+	fade_out_tween.play()
+	await fade_out_tween.finished
+	self.stop()
+	
+	# load new song
+	var songs_of_type:Array = audio_dict[song_type]
+	# gets a new random song
+	var song_path = songs_of_type.pick_random()
+	# loads it in
+	self.stream = load(song_path)
+	current_song_type_playing = song_type
+	self.play()
+	
+	# fade back in
+	var fade_in_tween = create_tween()
+	fade_in_tween.tween_property(self, "volume_db", 0, fade_duration)
+	fade_in_tween.play()
+
+func change_song_to_with_fade(song):
+	if not song:
+		change_song_with_fade()
+		return
+	
+	# fadeout
+	var fade_out_tween = create_tween()
+	fade_out_tween.tween_property(self, "volume_db", -80, fade_duration)
+	fade_out_tween.play()
+	await fade_out_tween.finished
+	self.stop()
+	
+	# loads it in
+	self.stream = song
+	current_song_type_playing = "custom"
+	self.play()
+	
+	# fade back in
+	var fade_in_tween = create_tween()
+	fade_in_tween.tween_property(self, "volume_db", 0, fade_duration)
+	fade_in_tween.play()
+	
+func change_song_instant():
+	
+	# first figure out song type
+	var song_type = "Background"
+	var songs_of_type:Array = audio_dict[song_type]
+	# gets a new random song
+	var song_path = songs_of_type.pick_random()
+	# loads it in
+	self.stream = load(song_path)
+	self.play()
+	
