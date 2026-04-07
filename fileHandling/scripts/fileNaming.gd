589d1b6e52e16d12606857415b0c6ca2f3fb00dc
linmjie <linmgjie@gmail.com>
2026-04-06 22:39:38 -0400

    added counter and then never incremented the counter

--- fileHandling/scripts/fileNaming.gd
+++ fileHandling/scripts/fileNaming.gd
@@ -1,1 +1,18 @@
 extends Object
+
+func makeUniquePath(path: String, fileLevel := true) -> String:
+	var checker: Callable = \
+		FileAccess.file_exists if fileLevel else DirAccess.dir_exists_absolute
+	if not checker.call(path): return path
+	var pathParts: PackedStringArray = path.split(".")
+	var base: String = pathParts[0]
+	var suffix: String = pathParts[-1]
+	var count: int = 1
+	while true:
+		var newPath = "%s-%d.%s" % [base, count, suffix]
+		if not checker.call(newPath): return newPath
+		count += 1
+	return path + "_WHY_CAN'T_WE_FIND_A_VALID_PATH_FOR_THIS_FILE"
+
+func getBackupPrefix() -> String:
+	return makeUniquePath("BACKUP_%s" % [Time.get_datetime_string_from_system()])
